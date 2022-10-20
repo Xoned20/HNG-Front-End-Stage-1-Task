92a6cd25e573f451f0d46e18c086997ff5da0a37
Jephery Cyril <cyriljeph@gmail.com>
2022-10-20 20:23:24 +0100

    creating react app and adding the main section

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Profile from "./components/Profile"
+import Link from "./components/Link"
+
+function Main() {
+    return(
+        <div>
+            <Profile />
+            <Link />
+        </div>
+    )
+}
+
+export default Main
