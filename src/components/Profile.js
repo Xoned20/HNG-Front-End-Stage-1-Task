491d05fc99068318c9aac228dc2c7a02c44b3ef3
Jephery Cyril <cyriljeph@gmail.com>
2022-10-20 21:22:28 +0100

    updating profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,20 @@
 import React from "react"
+import profileImg from "../images/profile__img.png"
 
 function Profile() {
     return(
         <div>
-            <img 
-            src=""
+            <img
+            id="profile__img"
+            src={profileImg}
             alt=""
             />
+            <p id="twitter">
+                Annette Black
+            </p>
+            <p id="slack">
+                cyriljephery
+            </p>
         </div>
     )
 }
