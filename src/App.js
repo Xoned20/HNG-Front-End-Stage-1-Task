45199886854bbe430ead63de9374c9236622abdd
Jephery Cyril <cyriljeph@gmail.com>
2022-10-27 01:04:33 +0100

    adding footer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import "./style.css"
 import Stage1 from "./components/Stage1"
+import Footer from "./components/Footer"
 
 function App(){
     return(
         <div>
             <Stage1/>
+            <Footer/>
         </div>
     )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Footer() {
+    return(
+        <div className="footer">
+            <div className="footer__container">
+                <div className="footer__content">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
