3a895b6302a3a8a32ebc95ccc4094e99b1cb72b2
Jephery Cyril <cyriljeph@gmail.com>
2022-10-27 01:23:37 +0100

    adding icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,21 @@
 import React from "react"
+import Zuri from "../images/Zuri.Internship_Logo.png"
+import I4G from "../images/I4G.png"
 
 function Footer() {
     return(
         <div className="footer">
             <div className="footer__container">
                 <div className="footer__content">
-                    
+                    <img 
+                    src={Zuri}
+                    alt=""
+                    />
+                    <p>HNG Internship 9 Frontend Task</p>
+                    <img 
+                    src={I4G}
+                    alt=""
+                    />
                 </div>
             </div>
         </div>
--- src/components/Stage1.js
+++ src/components/Stage1.js
@@ -2,51 +2,59 @@ import React from "react"
 import profileImg from "../images/profile__img.png"
 import Slack from "../images/slack.png"
 import Github from "../images/github.png"
+import Icon from "../images/Icon.png"
 
 function Stage1() {
     return(
-        <div className="stage1">
-            <div className="main__section">
-                {/* profile section */}
-                <section className="profile">
-                    <img
-                        id="profile__img"
-                        src={profileImg}
+        <div className="Stage1Web">
+            <div className="stage1">
+                <div className="main__section">
+                    {/* profile section */}
+                    <section className="profile">
+                        <img
+                            id="profile__img"
+                            src={profileImg}
+                            alt=""
+                        />
+                        <img
+                        className="icon"
+                        src={Icon}
                         alt=""
-                    />
-                    <p id="twitter">
-                    Annette Black
-                    </p>
-                    <p id="slack">
-                    {/* cyriljephery */}
-                    </p>
-                </section>
-                {/* profile section ends */}
+                        />
+                        <p id="twitter">
+                        Annette Black
+                        </p>
+                        <p id="slack">
+                        {/* cyriljephery */}
+                        </p>
+                    </section>
+                    {/* profile section ends */}
 
-                {/* links section */}
-                <section className="links">
-                    <button id="btn__zuri"><a href="https://training.zuri.team/">Twitter Link</a></button>
-                    <button><a>Zuri Team</a></button>
-                    <button id="books"><a href="http://books.zuri.team/">Zuri Books</a></button>
-                    <button id="book__python"><a href="https://books.zuri.team/python-for-beginners?ref_id=">Python Books</a></button>
-                    <button id="pitch"><a href="https://background.zuri.team/">Background Check for Coders</a></button>
-                    <button id="book__design"><a href="https://books.zuri.team/design-rules">Design Books</a></button>
-                </section>
-                {/* links section ends */}
-            </div>
-            
-            <div className="social">
-                <div className="social__container">
-                    <div className="social__content">
-                        <div className="social__icon">
-                            <img
-                            src={Slack}
-                            alt=""
-                            />
-                            <img
-                            src={Github}
-                            alt=""
-                            />
+                    {/* links section */}
+                    <section className="links">
+                        <button id="btn__zuri"><a href="https://training.zuri.team/">Twitter Link</a></button>
+                        <button><a>Zuri Team</a></button>
+                        <button id="books"><a href="http://books.zuri.team/">Zuri Books</a></button>
+                        <button id="book__python"><a href="https://books.zuri.team/python-for-beginners?ref_id=">Python Books</a></button>
+                        <button id="pitch"><a href="https://background.zuri.team/">Background Check for Coders</a></button>
+                        <button id="book__design"><a href="https://books.zuri.team/design-rules">Design Books</a></button>
+                    </section>
+                    {/* links section ends */}
+                </div>
+                
+                <div className="social">
+                    <div className="social__container">
+                        <div className="social__content">
+                            <div className="social__icon">
+                                <img
+                                src={Slack}
+                                alt=""
+                                />
+                                <img
+                                src={Github}
+                                alt=""
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
