5b9da08c8250a5b2e1fc3b22475db2c43500148d
Jephery Cyril <cyriljeph@gmail.com>
2022-11-01 00:58:54 +0100

    css commit

--- src/components/Stage1.js
+++ src/components/Stage1.js
@@ -15,18 +15,18 @@ function Stage1() {
                             id="profile__img"
                             src={profileImg}
                             alt=""
-                        />
+                        />        
+                        <p id="twitter">
+                        Annette Black
+                        </p>
+                        <p id="slack">
+                        {/* Xoned */}
+                        </p>
                         <img
                         className="icon"
                         src={Icon}
                         alt=""
                         />
-                        <p id="twitter">
-                        Annette Black
-                        </p>
-                        <p id="slack">
-                        {/* cyriljephery */}
-                        </p>
                     </section>
                     {/* profile section ends */}
 
@@ -35,7 +35,7 @@ function Stage1() {
                         <button id="btn__zuri"><a href="https://training.zuri.team/">Twitter Link</a></button>
                         <button><a>Zuri Team</a></button>
                         <button id="books"><a href="http://books.zuri.team/">Zuri Books</a></button>
-                        <button id="book__python"><a href="https://books.zuri.team/python-for-beginners?ref_id=">Python Books</a></button>
+                        <button id="book__python"><a href="https://books.zuri.team/python-for-beginners?ref_id=Xoned">Python Books</a></button>
                         <button id="pitch"><a href="https://background.zuri.team/">Background Check for Coders</a></button>
                         <button id="book__design"><a href="https://books.zuri.team/design-rules">Design Books</a></button>
                     </section>
